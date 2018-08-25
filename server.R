b638f2e494756832a5068ff08ffd5e501a696ea7
David del Moral <30639329+ddelmoral@users.noreply.github.com>
2018-08-25 16:14:37 -0300

    Resubmission of Aug 25, 2018
    Resubmission to address language observations made by peers.

--- server.R
+++ server.R
@@ -1,29 +1,24 @@
 # server.R
+
 library(shiny)
 library(ggplot2)
 library(dplyr)
 
-#setwd("~/Personal/Learning/Data Products/Project4")
 
-df<- read.csv(file="Contratos2018ANSI.csv", header=TRUE, sep=",")
+df<- read.csv(file="Compranet_2018_UTF8.csv", header=TRUE, sep="|")
 
-#summary(df)
-
-dfMXP <- subset(df, df$MONEDA == "MXN")
-
-
+dfMXP <- subset(df, df$CURRENCY == "MXP")
 
 server <- function(input, output) {
-  
   output$gplot <- renderPlot({
     filtered <-
       dfMXP %>%
-      filter(IMPORTE_CONTRATO >= input$range[1],
-             IMPORTE_CONTRATO <= input$range[2]
-            )
-      ggplot(filtered, aes(TIPO_CONTRATACION, IMPORTE_CONTRATO)) + 
-      geom_point(aes(col=TIPO_PROCEDIMIENTO)) +
-      xlab("Contract Type") +
-      ylab("Procurement Procedur")
-      })
+      filter(CONTRACT_AMOUNT >= input$range[1]*1000000,
+             CONTRACT_AMOUNT <= input$range[2]*1000000
+      )
+    ggplot(filtered, aes(CONTRACT_TYPE, CONTRACT_AMOUNT/1000000, col=CONTRACT_TYPE, size=CONTRACT_AMOUNT/1000000)) + 
+    geom_jitter() +
+    xlab("Contract Type") +
+    ylab("Contract Amount Millions Mexican Pesos") 
+  })
 }
--- ui.R
+++ ui.R
@@ -1,20 +1,17 @@
 # ui.R
 library(shiny)
 
-#setwd("~/Personal/Learning/Data Products/Project4")
-
-
-# Define UI for miles per gallon app ----
 ui <- fluidPage(
+  
+  titlePanel("Mexican Government Acquisitions 2018 (Millions of MXP)"),
+  
+  sliderInput("range", "Contract Amount Range:",
+              min = 1, max = 1000,
+              value = c(100,750)),
+  
+  plotOutput("gplot")
+)
 
-    titlePanel("Mexican Government Acquisitions 2018 (MXP)"),
-    
-    sliderInput("range", "Range:",
-          min = 1, max = 1000000000,
-          value = c(1000000,500000000)),
-    
-    plotOutput("gplot")
-  )
 
 
 
